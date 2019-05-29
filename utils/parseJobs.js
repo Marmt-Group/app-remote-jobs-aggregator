b93e126d7ee5627c9c3949085b6d95c73d13ad80
David Davis <davidjamesdavis.djd@gmail.com>
2019-05-28 21:29:59 -0700

    identify most recent jobs

--- utils/parseJobs.js
+++ utils/parseJobs.js
@@ -3,6 +3,8 @@
  */
 const parseJobs = (feeds, jobQuery) => {
 
+    const date = new Date(); 
+
     const sortByDate = ((a, b) => {
         if (a.date) {
             return new Date(b.date) - new Date(a.date)
@@ -18,17 +20,23 @@ const parseJobs = (feeds, jobQuery) => {
             case 'full stack':
                 return value.title.match(/Full Stack|full stack|Full-stack|full-stack|Senior Full Stack|senior full stack/g)
             case 'front end':
-                return value.title.match(/Front End|front end|Front-end|front-end|Senior Front End|Sr. Front End/g)
+                return value.title.match(/Front End|front end|Front-end|front-end|Frontend|Senior Front End|Sr. Front End/g)
             default:
-                return value.title.match(/Front End|front end|Front-end|front-end|Senior Front End|Sr. Front End/g)
+                return value.title.match(/Front End|front end|Front-end|front-end|Frontend|Senior Front End|Sr. Front End/g)
         } 
     }
+
+    feeds.github.value.forEach(element => {
+        const date = new Date(element.created_at);
+        element.created_at = date
+    })
 
     return {
         github: feeds.github.value.filter(sortByTitle).sort(sortByDate), 
         wwremotely: feeds.wwremotely.value.items.filter(sortByTitle).sort(sortByDate),
         stackoverflow: feeds.stackoverflow.value.items.filter(sortByTitle).sort(sortByDate),
-        codepen: feeds.codepen.value.jobs.filter(sortByRemote).filter(sortByTitle)
+        codepen: feeds.codepen.value.jobs.filter(sortByRemote).filter(sortByTitle),
+        recentDate: date.setDate(date.getDate() - 10)
     }
 }
 
